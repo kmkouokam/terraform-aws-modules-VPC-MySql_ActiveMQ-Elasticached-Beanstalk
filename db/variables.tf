f9a807046c17d1ea5f1f0d2299a31a51951024e3
kmkouokam <94249111+kmkouokam@users.noreply.github.com>
2024-03-04 13:41:41 -0500

    Update variables.tf

--- db/variables.tf
+++ db/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
-  description = "Region where you want to provision the Servers "
+  description = "Region where you want to provision the Servers"
   default     = "us-east-1"
 }
 
 
 variable "elasticache_subnet_group_names" {
-  description = "The list of names for Elasticached subnet groups"
+  description = "The names for Elasticached subnet groups"
   default = [
     "cache-subnet-group1",
     "cache-subnet-group2",
